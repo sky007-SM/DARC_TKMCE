522f788df2f1f394d6556ff76691e9f969b03e91
Sky <niranjanunnikrishnan2007@gmail.com>
2026-06-19 10:33:16 +0530

    final website

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -12,22 +13,25 @@ import About from './pages/About'
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* handles scroll reset + hash navigation */}
-      <ScrollToTop />
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
 
-      <Navbar />
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects/:slug" element={<ProjectDetail />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects/:slug" element={<ProjectDetail />} />
+        </Routes>
 
-      <Footer />
-    </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+
+      <Analytics />
+    </>
   )
 }
 
